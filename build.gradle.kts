32164af10226ac85d902e0d83c46f27768ee9009
Johnny Wulgaru <wulgaru@hispeed.ch>
2025-12-06 08:05:54 +0100

    build improvements, so we can run everything from CLI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,22 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    application
+}
+
+kotlin {
+    jvmToolchain(24)
 }
 
 sourceSets {
     main {
         kotlin.srcDir("src")
     }
+}
+
+val day: String? by project
+
+application {
+    mainClass.set("Day${day?.padStart(2, '0') ?: "01"}Kt")
 }
 
 tasks {
